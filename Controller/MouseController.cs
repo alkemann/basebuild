8e8a249111476080057308083c333d8676dc26ec
Alexander Morland <alek.morland@gmail.com>
2016-01-29 00:08:51 +0100

    Camera movement and tile hover cursor

--- Controller/MouseController.cs
+++ Controller/MouseController.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class MouseController : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject cursor_go;
+
+	Camera cam;
+	Vector3 lastFramePosition;
+	Vector3 currFramePosition;
+
 	void Start () {
-	
+		cam = Camera.main;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		currFramePosition = cam.ScreenToWorldPoint (Input.mousePosition);
+		currFramePosition.z = 0;
+
+		cameraMovement ();
+		cursor ();
+
+		lastFramePosition =  cam.ScreenToWorldPoint (Input.mousePosition);
+		lastFramePosition.z = 0;
+	}
+
+	void cameraMovement ()
+	{
+		if ( Input.GetMouseButton(1) || Input.GetMouseButton(2) ) { // right or middle button
+			cam.transform.Translate (lastFramePosition - currFramePosition);
+		}
+	}
+
+	void cursor ()
+	{
+		// cursor_go.transform.position = currFramePosition; // moves  cursor to freeform cursor
+		Tile t = getTileAtWorldCoord(currFramePosition);
+		if (t == null) {
+			cursor_go.SetActive (false);
+		} else {
+			cursor_go.SetActive (true);
+			cursor_go.transform.position = new Vector3 (t.x, t.y, 0);
+		}
+	}
+
+
+	Tile getTileAtWorldCoord(Vector3 coord)
+	{
+		int x = Mathf.FloorToInt (currFramePosition.x);
+		int y = Mathf.FloorToInt (currFramePosition.y);
+		return GetComponent<WorldController> ().getTileAt (x, y);
 	}
 }
--- Controller/WorldController.cs
+++ Controller/WorldController.cs
@@ -5,15 +5,14 @@ public class WorldController : MonoBehaviour {
 
 	World world;
 
-	void Start () {
-		world = new World ();
-		SpritesController sc = GetComponentInParent<SpritesController> ();
-		sc.renderTiles (world);
-	}
-
-	void Update()
+	void Start ()
 	{
-
+		world = new World ();
+		GetComponentInParent<SpritesController> ().renderTiles (world);
 	}
-		
+
+	public Tile getTileAt(int x, int y)
+	{
+		return world.getTileAt (x, y);
+	}
 }
