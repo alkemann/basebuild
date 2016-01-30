cff74807247e072e48bd645436d0331339c61193
Alexander Morland <alek.morland@gmail.com>
2016-01-30 16:03:13 +0100

    Added menu scripts

---
+++ Utilities/AutomaticVerticalSize.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutomaticVerticalSize : MonoBehaviour {
+
+	public float childHeight = 35f;
+	public float padding = 2f;
+
+	void Start() {
+		adjustSize ();
+	}
+
+	public void adjustSize ()
+	{
+		RectTransform tran = this.GetComponent<RectTransform> ();
+		Vector2 size = tran.sizeDelta;
+		int children = this.transform.childCount;
+		size.y = children * childHeight + ((children - 1) * padding);
+		tran.sizeDelta = size;
+	}
+}
---
+++ Utilities/Editor/UpdateButtonPanelEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(AutomaticVerticalSize))]
+public class UpdateButtonPanel : Editor
+{
+
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		if (GUILayout.Button ("Refresh menu")) {
+			((AutomaticVerticalSize)target).adjustSize ();
+		}
+	}
+}
