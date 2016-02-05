14ec6e59a0652c67977cd3f7c113bbe17382f665
Alexander Morland <alek.morland@gmail.com>
2016-02-05 01:20:02 +0100

    PriotyQueued A* pathfinding

--- Model/Pathfinder.cs
+++ Model/Pathfinder.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using Path;
 
 public class Pathfinder 
 {
 
 	World world;
-	List<Tile> traversable_graph;
+	TileGraph tileGraph;
 
 	public Pathfinder(World world)
 	{
-		traversable_graph = new List<Tile> ();
-
-		world.registerOnTileChanged (tileChanged); // listen to all tile changes
+		this.world = world;
+		world.registerOnTileChanged (tileChanged); // listen to all tile changes to recreate graph
+		// FIXME delay on so that many changes doesnt cause constant re-graphing
 	}
 
 	/**
@@ -19,122 +20,14 @@ public class Pathfinder
 	 */
 	protected void tileChanged(Tile t)
 	{
-		if (t.type == Tile.TYPE.EMPTY || (t.Furniture != null && t.Furniture.type == Furniture.TYPE.WALL)) {
-			// should NOT be in traversable graph
-			if (traversable_graph.Contains (t))
-				traversable_graph.Remove (t);
-		} else {
-			// should be in graph
-			if (traversable_graph.Contains (t) == false)
-				traversable_graph.Add(t);
-		}
+		tileGraph = null;
 	}
 
 	public List<Tile> findPath(Tile from, Tile to)
 	{
-		return astar_search (from, to);
-	}
-
-
-
-	protected List<Tile> astar_search (Tile start, Tile goal)
-	{
-		List<Tile> frontier = new List<Tile>(traversable_graph.Count);
-		Dictionary<Tile, Tile> cameFrom = new Dictionary<Tile, Tile>(traversable_graph.Count / 2);
-		Dictionary<Tile, float> cost_so_far = new Dictionary<Tile, float>(traversable_graph.Count / 2);
-
-		frontier.Add(start);
-
-		cameFrom[start] = start;
-		cost_so_far[start] = 0;
-
-		while (frontier.Count > 0) {
-			Tile current = null;
-
-			// This is instead of a priorty queue
-			foreach (Tile possibleCurrent in frontier) {
-				if (current == null ||  cost_so_far [possibleCurrent] < cost_so_far [current])
-					current = possibleCurrent;
-			}
-
-			if (current == goal) {
-				break; // we found the path
-			}
-
-			foreach (Tile next in current.getConnected()) {
-				float newCost = cost_so_far[current] + next.costToEnterFrom(current.X, current.Y);
-				if (!cost_so_far.ContainsKey(next) || newCost < cost_so_far[next]) {
-					cost_so_far[next] = newCost;
-					float priority = newCost + heuristic_cost_estimate(next, goal);
-//					frontier.Add(next);
-					cameFrom[next] = current;
-				}
-			}
-		}
-	}
-
-
-
-	protected List<Tile> dijikstra_search (Tile source, Tile target)
-	{
-		List<Tile> frontier = new List<Tile>(traversable_graph.Count);
-		Dictionary<Tile, float> cost_so_far = new Dictionary<Tile, float> (traversable_graph.Count);
-		Dictionary<Tile, Tile> came_from = new Dictionary<Tile, Tile> (traversable_graph.Count);
-
-		// Initialization
-		foreach (Tile t in traversable_graph) {
-			cost_so_far[t] = Mathf.Infinity; 	// Unknown distance from source to v
-			came_from[t] = null; 				// Previous node in optimal path from source
-			frontier.Add(t); 					// All nodes initially in unvisited nodes
-		}
-
-		cost_so_far[source] = 0;
-		while (frontier.Count > 0) {
-			Tile current = null;
-
-			// This is instead of a priorty queue
-			foreach (Tile possibleCurrent in frontier) {
-				if (current == null ||  cost_so_far [possibleCurrent] < cost_so_far [current])
-					current = possibleCurrent;
-			}
-
-			if (current == target) {
-				break; // we found the path
-			}
-
-			foreach (Tile next in current.getConnected()) { // where v is still in unvisited.
-				if (current.isPassable() == false && current != source ) continue;
-				if (came_from.ContainsKey(next) == false) continue;
-				float new_cost = cost_so_far[current] + next.costToEnterFrom(current.X, current.Y);
-				if (new_cost < cost_so_far[next]) { // A shorter path to v has been found
-					cost_so_far[next] = new_cost;
-					came_from[next] = current;
-
-					// add back next to frontier?
-				}
-			}
-
-			frontier.Remove (current); // ie remove possibleCurrent from the frontier
-		}
-
-		if (came_from [target] == null) { // No path to target could be found
-			Debug.Log ("No route found");
-			return null;
-		}
-		List<Tile> path = new List<Tile> ();
-		Tile curr = target;
-		while (curr != null) {  // Construct the shortest path with a stack S
-			path.Add (curr); 	// Push the vertex onto the stack
-			curr = came_from [curr];	// Traverse from target to source
-		}
-
-		return path;
-	}
-
-	
-	protected int heuristic_cost_estimate (Tile start, Tile goal)
-	{
-		return Mathf.Abs(start.X-goal.X) + Mathf.Abs(start.Y-goal.Y);
+		if (tileGraph == null)
+			tileGraph = new TileGraph (world);
+		return tileGraph.search(from, to);
 	}
 
 }
---
+++ Utilities/Path/TileGraph.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Path {
+
+
+	public class RoomGraph
+	{
+
+	}
+
+	public class TileGraph
+	{
+		Dictionary<Tile, Node<Tile>> nodes;
+
+		public TileGraph(World world)
+		{
+			nodes = new Dictionary<Tile, Node<Tile>> (10);
+
+			// create nodes for tiles that are passable and are not in space
+			for (int x = 0; x < world.Width; x++) {
+				for (int y = 0; y < world.Height; y++) {
+					Tile t = world.getTileAt (x, y);
+					if (t.type != Tile.TYPE.EMPTY) { //  && t.isPassable ()
+						nodes.Add (t, new Node<Tile> (t));
+					}
+				}
+			}
+
+			// foreach node
+			foreach (Node<Tile> node in nodes.Values) {
+				Tile tile = node.data;
+				// get connected tiles
+				foreach (Tile connected in tile.getConnected()) {
+					// neighbor is not in space or not passable, add edge to it
+					if (connected.type != Tile.TYPE.EMPTY ) { // && connected.isPassable ()
+						node.edges.Add (new Edge<Tile> (nodes[connected], connected.costToEnterFrom (tile.X, tile.Y)));
+					}
+				}
+			}
+		}
+
+		public List<Tile> search(Tile from, Tile target)
+		{
+			Node<Tile> start = nodes [from];
+			Node<Tile> goal = nodes [target];
+			PriorityQueue<Node<Tile>> frontier = new PriorityQueue<Node<Tile>>();
+			frontier.put (start, 0);
+
+			Dictionary<Node<Tile>, Node<Tile>> came_from = new Dictionary<Node<Tile>, Node<Tile>> (); // Assume we must travel half nodes
+			Dictionary<Node<Tile>, float> cost_so_far = new Dictionary<Node<Tile>, float>();    // nodes.Count / 2
+
+			came_from [start] = null;
+			cost_so_far [start] = 0;
+
+			while (frontier.Count > 0) {
+				Node<Tile> current = frontier.get ();
+
+				if (current == goal)
+					break; // found our goal
+
+				foreach (Edge<Tile> edge in current.edges) {
+					Node<Tile> next = edge.node;
+					float new_cost = cost_so_far [current] + edge.cost;
+					if (cost_so_far.ContainsKey (next) == false || new_cost < cost_so_far [next]) {
+						cost_so_far [next] = new_cost;
+						int priority = (int)new_cost + heuristic_cost_estimate (next.data, target);
+						frontier.put (next, priority);
+						came_from [next] = current;
+					}
+				}
+			}
+
+			if (came_from [goal] == null) {
+				Debug.Log ("No path found");
+				return null;
+			}
+
+			List<Tile> path = new List<Tile> ();
+			Node<Tile> iterator = goal;
+			do {
+				path.Add(iterator.data);
+				iterator = came_from[iterator];
+			} while (iterator != null);
+			return path;
+		}
+
+		protected int heuristic_cost_estimate (Tile start, Tile goal)
+		{
+			return Mathf.Abs(start.X-goal.X) + Mathf.Abs(start.Y-goal.Y);
+		}
+
+	}
+
+	class Node<T>
+	{
+		public T data;
+		public List<Edge<T>> edges;
+
+		public Node(T data)
+		{
+			this.data = data;
+			edges = new List<Edge<T>> (8);
+		}
+	}
+
+	class Edge<T>
+	{
+		public Node<T> node;
+		public float cost;
+
+		public Edge (Node<T> node, float cost)
+		{
+			this.node = node;
+			this.cost = cost;
+		}
+
+	}
+
+}
