81e2d40204c832fbd376f9291ce188eb7a755a46
Alexander Morland <alek.morland@gmail.com>
2016-03-04 22:12:06 +0100

    Added Tuple3

---
+++ Utilities/Tuple3.cs
@@ -0,0 +1,21 @@
+using System.Xml.Serialization;
+
+public class Tuple3<T1, T2, T3>
+{
+	[XmlAttribute]
+	public T1 item1;
+	[XmlAttribute]
+	public T2 item2;
+	[XmlAttribute]
+	public T3 item3;
+
+	public Tuple3 () { }
+
+	public Tuple3 (T1 t1, T2 t2, T3 t3)
+	{
+		this.item1 = t1;
+		this.item2 = t2;
+		this.item3 = t3;
+	}
+}
+
